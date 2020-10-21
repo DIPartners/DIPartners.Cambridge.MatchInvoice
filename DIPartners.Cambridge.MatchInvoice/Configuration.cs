1344cc11c2164a760181c5bb6ddf83b0be107f78
energykhj <heuijin.ko@gmail.com>
2020-10-21 12:07:30 -0400

    Create project

---
+++ DIPartners.Cambridge.MatchInvoice/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MFiles.VAF.Configuration;
+using MFiles.VAF.Configuration.JsonAdaptor;
+
+namespace DIPartners.Cambridge.MatchInvoice
+{
+    [DataContract]
+    public class Configuration
+    {
+    }
+}
